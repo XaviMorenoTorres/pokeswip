860288da041735da1791ca6c13dd9cc15907b5e1
XaviMorenoTorres <96111285+XaviMorenoTorres@users.noreply.github.com>
2021-12-14 13:36:45 +0100

    s

--- js/code.js
+++ js/code.js
@@ -32,7 +32,7 @@ function getListRandom(n, min, max) {
 
 function galleryRandom() {
     let min = 1;
-    let max = 10;
+    let max = 17;
     let imagenes = document.getElementsByTagName("img")
     let listRnd = getListRandom(imagenes.length - 1, min, max)
     for (let i = 0; i < imagenes.length - 1; i++) {
